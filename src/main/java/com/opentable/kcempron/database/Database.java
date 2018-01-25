c7d80da67c802d02482653e1274ffac6bf48fab4
Karl Cempron <cempronkarl@gmail.com>
2018-01-25 15:09:16 -0800

    Added Profiles

--- src/main/java/com/opentable/kcempron/database/Database.java
+++ src/main/java/com/opentable/kcempron/database/Database.java
@@ -9,18 +9,19 @@ import java.util.Map;
 public class Database {
 
     private static Map<Long, Message> messages = new HashMap<>();
-    private static Map<Long, Profile> profiles = new HashMap<>();
+    private static Map<String, Profile> profiles = new HashMap<>();
 
     static {
         messages.put(1L, new Message(1, "Hello World", "kcempron"));
         messages.put(2L, new Message(2, "Hello OpenTable", "kcempron"));
+        profiles.put("kcempron", new Profile(1L, "kcempron", "Karl", "Cempron"));
     }
 
     public static Map<Long, Message> getMessages() {
         return messages;
     }
 
-    public static Map<Long, Profile> getProfiles() {
+    public static Map<String, Profile> getProfiles() {
         return profiles;
     }
 }
