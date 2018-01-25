5382959fc0c1e330df32c22ecb4a702c48627e10
Karl Cempron <cempronkarl@gmail.com>
2018-01-25 15:10:12 -0800

    Added service methods for Profile

---
+++ src/main/java/com/opentable/kcempron/service/ProfileService.java
@@ -0,0 +1,42 @@
+package com.opentable.kcempron.service;
+
+import com.opentable.kcempron.database.Database;
+import com.opentable.kcempron.model.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ProfileService {
+
+    public ProfileService() { }
+
+    private Map<String, Profile> profiles = Database.getProfiles();
+
+    public List<Profile> getAllProfiles() {
+        return new ArrayList<Profile>(profiles.values());
+    }
+
+    public Profile getProfile(String profileName) {
+        return profiles.get(profileName);
+    }
+
+    public Profile addProfile(Profile profile) {
+        profile.setId(profiles.size() + 1);
+        profiles.put(profile.getProfileName(), profile);
+        return profile;
+    }
+
+    public Profile updateProfile(Profile profile) {
+        if (profile.getProfileName().isEmpty()) {
+            return null;
+        }
+
+        profiles.put(profile.getProfileName(), profile);
+        return profile;
+    }
+
+    public Profile removeProfile(String profileName) {
+        return profiles.remove(profileName);
+    }
+}
