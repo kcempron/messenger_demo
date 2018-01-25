10957523e9906ceb781749c1c56aaf6a83f5f0ca
Karl Cempron <cempronkarl@gmail.com>
2018-01-25 14:30:23 -0800

    Cleaned up request annotations

--- src/main/java/com/opentable/kcempron/resources/MessageResource.java
+++ src/main/java/com/opentable/kcempron/resources/MessageResource.java
@@ -8,34 +8,30 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
     }
 
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Message test(@PathParam("messageId") long messageId) {
         return messageService.getMessage(messageId);
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Message addMessage(Message message) {
         return messageService.addMessage(message);
     }
 
     @PUT
     @Path("/{messageId}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
         message.setId(messageId);
         return messageService.updateMessage(message);
@@ -43,7 +39,6 @@ public class MessageResource {
 
     @DELETE
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public void deleteMessage(@PathParam("messageId") long id) {
         messageService.removeMessage(id);
     }
