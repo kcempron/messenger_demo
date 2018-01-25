7842e68f7d6b4f8248c524f8047e3def7c1b8047
Karl Cempron <cempronkarl@gmail.com>
2018-01-25 13:36:56 -0800

    changed result to JSON format

--- src/main/java/com/opentable/kcempron/resources/MessageResource.java
+++ src/main/java/com/opentable/kcempron/resources/MessageResource.java
@@ -5,6 +5,7 @@ import com.opentable.kcempron.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -15,8 +16,15 @@ public class MessageResource {
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
+    }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message test(@PathParam("messageId") long messageId) {
+        return messageService.getMessage(messageId);
     }
 }
